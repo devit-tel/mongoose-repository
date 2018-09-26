78ee4eada52dd856f569d197671efd5652dff96f
NV4RE <tosbodesz@gmail.com>
2018-09-26 17:59:42 +0700

    Fix .d.ts

--- src/repositoryBuilder/index.ts
+++ src/repositoryBuilder/index.ts
@@ -1,17 +1,16 @@
-const mongoose = require('mongoose')
-const MongooseBaseRepository = require('../index')
-const mongoosePaginate = require('mongoose-paginate')
-const mongooseTimestamps = require('mongoose-timestamp')
-const mongooseDelete = require('mongoose-delete')
+import * as mongoose from 'mongoose'
+import MongooseBaseRepository from '../index'
+import * as mongoosePaginate from 'mongoose-paginate'
+import * as mongooseTimestamps from 'mongoose-timestamp'
+import * as mongooseDelete from 'mongoose-delete'
 
-export default (modelName: String, schemaDefinition: any, collectionName: any) => {
+export default (modelName: string, schemaDefinition: any, collectionName: string) => {
   const Schema = new mongoose.Schema(schemaDefinition)
   Schema.plugin(mongooseDelete)
   Schema.plugin(mongooseTimestamps)
   Schema.plugin(mongoosePaginate)
 
   const Model = mongoose.model(modelName, Schema, collectionName)
-
   const Repository = new MongooseBaseRepository(Model)
 
   return {
