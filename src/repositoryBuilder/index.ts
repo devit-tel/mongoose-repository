6cd2d64a98c3de132d0a5e41d2260bc1a0fe076b
NV4RE <tosbodesz@gmail.com>
2018-10-26 16:34:20 +0700

    Add SchemaConfigs

--- src/repositoryBuilder/index.ts
+++ src/repositoryBuilder/index.ts
@@ -4,11 +4,34 @@ import * as mongoosePaginate from 'mongoose-paginate'
 import * as mongooseTimestamps from 'mongoose-timestamp'
 import * as mongooseDelete from 'mongoose-delete'
 
-export default (modelName: string, schemaDefinition: any) => {
+interface SchemaPlugin {
+  plugin: any,
+  options: any
+}
+
+interface SchemaIndexs {
+  fields: {
+    [key: string] : number
+  },
+  options: any
+}
+
+interface SchemaConfig {
+  plugins?: [SchemaPlugin]
+  indexs?: [SchemaIndexs]
+} // You can create more interface like set, pre, N/A here
+
+export default (modelName: string, schemaDefinition: any, schemaConfig: SchemaConfig = {}) => {
   const Schema = new mongoose.Schema(schemaDefinition)
   Schema.plugin(mongooseDelete, { deletedAt: true, indexFields: true, overrideMethods: true })
   Schema.plugin(mongooseTimestamps)
   Schema.plugin(mongoosePaginate)
+  if(Array.isArray(schemaConfig.plugins)) {
+    schemaConfig.plugins.map(({ plugin, options }) => Schema.plugin(plugin, options))
+  }
+  if(Array.isArray(schemaConfig.indexs)) {
+    schemaConfig.indexs.map(({ fields, options }) => Schema.index(fields, options))
+  }
 
   const Model = mongoose.model(modelName, Schema)
   const Repository = new MongooseBaseRepository(Model)
