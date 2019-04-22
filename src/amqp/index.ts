e850218f53d58eb30fda5eb7e7c72bc71ffc99a4
Chanatip Deenang <chanatip@110-170-29-81.static.asianet.co.th>
2019-04-22 13:45:15 +0700

    check env AMQP_URI

--- src/amqp/index.ts
+++ src/amqp/index.ts
@@ -4,9 +4,6 @@ declare var process: {
   env: {
     NODE_ENV: string,
     AMQP_URI: string,
-    AMQP_URI1: string,
-    AMQP_URI2: string,
-    AMQP_URI3: string,
     AMQP_USERNAME: string,
     AMQP_PASSWORD: string,
     AMQP_PORT: number,
@@ -19,22 +16,19 @@ const queueNameCreate = `${process.env.NODE_ENV}.${routingKey}.create`
 const queueNameUpdate = `${process.env.NODE_ENV}.${routingKey}.update`
 const exchangeName = `${process.env.NODE_ENV}.mongoose-repository.caller`
 const isProduction = process.env.NODE_ENV === 'production'
+const amqpUrl = process.env.AMQP_URI ? process.env.AMQP_URI.split(',') : null
 
 let configHost = {}
 if (isProduction) {
   configHost = {
-    connections: [
-      `amqp://${process.env.AMQP_USERNAME}:${process.env.AMQP_PASSWORD}@${process.env.AMQP_URI1}:${process.env.AMQP_PORT}//${process.env.NODE_ENV}?heartbeat=5`,
-      `amqp://${process.env.AMQP_USERNAME}:${process.env.AMQP_PASSWORD}@${process.env.AMQP_URI2}:${process.env.AMQP_PORT}//${process.env.NODE_ENV}?heartbeat=5`,
-      `amqp://${process.env.AMQP_USERNAME}:${process.env.AMQP_PASSWORD}@${process.env.AMQP_URI3}:${process.env.AMQP_PORT}//${process.env.NODE_ENV}?heartbeat=5`
-    ]
+    connections: amqpUrl
   }
 } else {
   configHost = {
     connection: {
       slashes: true,
       protocol: 'amqp',
-      hostname: process.env.AMQP_URI,
+      hostname: amqpUrl ? amqpUrl[0] : null,
       user: process.env.AMQP_USERNAME,
       password: process.env.AMQP_PASSWORD,
       vhost: `//${process.env.NODE_ENV}`,
